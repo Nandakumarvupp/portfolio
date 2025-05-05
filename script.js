3b7f7165808558c67891fd652156d17c5a03ae40
Nandakumarvupp <168692523+Nandakumarvupp@users.noreply.github.com>
2025-05-04 22:33:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
 // Smooth scrolling for blog links
 document.querySelectorAll('.blog-link').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
